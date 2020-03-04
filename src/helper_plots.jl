8a55ad6f5cedd4c4a0d01f41ddc02b820269f94d
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-03-03 19:11:05 -0800

    add helper plot function

---
+++ src/helper_plots.jl
@@ -0,0 +1,31 @@
+@userplot ConfIntPlot
+
+
+@recipe function f(c::ConfIntPlot)
+
+      if length(c.args) == 2
+            targets, cis = c.args
+            estimated_targets = mean.(cis)
+      elseif length(c.args) == 3
+             targets, cis, estimated_targets = c.args
+      else
+             error("Expected 2 or 3 arguments")
+      end
+
+      xs = response.(targets)
+
+      left_ci = first.(cis)
+      right_ci = last.(cis)
+
+      lower_lims = estimated_targets .- left_ci
+      upper_lims = right_ci .- estimated_targets
+
+      ribbons --> (lower_lims, upper_lims)
+      fillalpha --> 0.4
+
+
+      xlab --> L"\mu"
+      ylab --> pretty_label(targets[1])
+      
+      xs, estimated_targets
+end
