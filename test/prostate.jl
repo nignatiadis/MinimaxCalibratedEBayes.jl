889d7126e77223ef6134785bf0e187928908d716
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-12-15 08:31:11 -0800

    update prostate code

--- test/prostate.jl
+++ test/prostate.jl
@@ -32,27 +32,30 @@ R"load($prostz_file)"
 Zs = StandardNormalSample.(prostz)
 
 gcal_scalemix = Empirikos.set_defaults(GaussianScaleMixtureClass(), Zs; hints = Dict(:grid_scaling => 1.1))
-gcal_locmix = MixturePriorClass(Normal.(-3:0.05:3, 0.2))
+gcal_locmix = MixturePriorClass(Normal.(-3:0.05:3, 0.25))
 
 
 dkw_nbhood = DvoretzkyKieferWolfowitz(0.05)
 
 fitted_dkw = fit(dkw_nbhood, Zs)
-x_dkw = collect(keys(fitted_dkw.summary))
-F_dkw = collect(values(fitted_dkw.summary))
-plot(response.(x_dkw), F_dkw, ribbon = fitted_dkw.band, label=nothing)
+plot(fitted_dkw, subsample=300, label="DKW band",
+     xlab=L"x", ylab=L"\widehat{F}_n(x)",  size=(380,280))
+savefig("prostate_dkw_band.tikz")
 
-
-infty_nbhood = Empirikos.InfinityNormDensityBand(a_min=-4.0,a_max=4.0);
+infty_nbhood = Empirikos.InfinityNormDensityBand(a_min=-3.0,a_max=3.0);
 fitted_infty_nbhood = fit(infty_nbhood, Zs)
 fitted_infty_nbhood.C∞
 
+
+
 prostate_marginal_plot = histogram(response.(Zs), bins=50, normalize=true,
-          label="", alpha=0.4, fillcolor=:lightgray,
-          grid=nothing, size=(500,350), xlim=(-4,4))
-
-plot!(response.(fitted_infty_nbhood.midpoints), fitted_infty_nbhood.estimated_density, ribbon=fitted_infty_nbhood.C∞)
-
+          label="Histogram", fillalpha=0.4, linealpha=0.4, fillcolor=:lightgray,
+          size=(380,280), xlims=(-4.5,4.5))
+plot!(prostate_marginal_plot, fitted_infty_nbhood,
+      label="KDE band", xlims=(-4.5,4.5),
+      yguide=L"\widehat{f}_n(x)", xguide=L"x")
+plot!([-3.0;3.0], seriestype=:vline, linestyle=:dot, label=nothing, color=:lightgrey)
+savefig("prostate_kde_band.tikz")
 
 nbhood_method_dkw_scalemix = NeighborhoodWorstCase(neighborhood = fitted_dkw,
                                convexclass = gcal_scalemix, solver = quiet_mosek)
@@ -68,7 +71,7 @@ nbhood_method_kde_locmix = NeighborhoodWorstCase(neighborhood = fitted_infty_nbh
                                convexclass = gcal_locmix, solver = quiet_mosek)
 
 
-discr = Empirikos.Discretizer(-4:0.01:4)
+discr = Empirikos.Discretizer(-3.0:0.005:3.0)
 
 lam_kde_scalemix = Empirikos.LocalizedAffineMinimax(neighborhood = (@set infty_nbhood.α=0.01),
                             discretizer=discr,
@@ -84,46 +87,56 @@ lfsrs = Empirikos.PosteriorProbability.(StandardNormalSample.(ts), Interval(0,no
 postmean_targets = Empirikos.PosteriorMean.(StandardNormalSample.(ts))
 
 
+# Posterior Mean
+
 postmean_ci_dkw_scalemix = confint.(nbhood_method_dkw_scalemix, postmean_targets, Zs)
 postmean_ci_kde_scalemix = confint.(nbhood_method_kde_scalemix, postmean_targets, Zs)
 postmean_ci_lam_scalemix = confint.(lam_kde_scalemix, postmean_targets, Zs)
 
-plot(ts, postmean_ci_kde_scalemix, label="KDE-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(-2,2))
+plot(ts, postmean_ci_kde_scalemix, label="KDE-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55))
 plot!(ts, postmean_ci_dkw_scalemix, label="DKW-Loc",show_ribbon=false, alpha=0.9, color=:black)
 plot!(ts, postmean_ci_lam_scalemix, label="Affine minimax",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
 plot!([-3.0;3.0], [-3.0; 3.0], seriestype=:line, linestyle=:dot, label=nothing, color=:lightgrey)
-plot!(xlabel = L"x", ylabel=L"\EE{\mu \mid X=x}", size=(380,250))
+plot!(xlabel = L"x", ylabel=L"\EE{\mu \mid X=x}", size=(380,280))
 
 savefig("prostate_scalemix_postmean.tikz")
 
-plot!(ts, postmean_targets.(prior))
 
 postmean_ci_dkw_locmix = confint.(nbhood_method_dkw_locmix, postmean_targets, Zs)
 postmean_ci_kde_locmix = confint.(nbhood_method_kde_locmix, postmean_targets, Zs)
-
 postmean_ci_lam_locmix = confint.(lam_kde_locmix, postmean_targets, Zs)
 
-confint(nbhood_method_kde_locmix, Empirikos.PosteriorMean.(StandardNormalSample.(0.0)), Zs)
+plot(ts, postmean_ci_kde_locmix, label="KDE-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55))
+plot!(ts, postmean_ci_dkw_locmix, label="DKW-Loc",show_ribbon=false, alpha=0.9, color=:black)
+plot!(ts, postmean_ci_lam_locmix, label="Affine minimax",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
+plot!([-3.0;3.0], [-3.0; 3.0], seriestype=:line, linestyle=:dot, label=nothing, color=:lightgrey)
+plot!(xlabel = L"x", ylabel=L"\EE{\mu \mid X=x}", size=(380,280))
+
+savefig("prostate_locmix_postmean.tikz")
+
+# Local false sign rate
+
+lfsr_ci_dkw_scalemix = confint.(nbhood_method_dkw_scalemix, lfsrs, Zs)
+lfsr_ci_kde_scalemix = confint.(nbhood_method_kde_scalemix, lfsrs, Zs)
+lfsr_ci_lam_scalemix = confint.(lam_kde_scalemix, lfsrs, Zs)
+
+plot(ts, lfsr_ci_kde_scalemix, label="KDE-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(0,1))
+plot!(ts, lfsr_ci_dkw_scalemix, label="DKW-Loc",show_ribbon=false, alpha=0.9, color=:black)
+plot!(ts, lfsr_ci_lam_scalemix, label="Affine minimax",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
+plot!([-3;3], [0.5; 0.5], seriestype=:line, linestyle=:dot, label=nothing, color=:lightgrey)
+plot!(xlabel = L"x", ylabel=L"\PP{\mu \geq 0 \mid X=x}", size=(380,280))
+
+savefig("prostate_scalemix_lfsr.tikz")
 
 
+lfsr_ci_dkw_locmix = confint.(nbhood_method_dkw_locmix, lfsrs, Zs)
+lfsr_ci_kde_locmix = confint.(nbhood_method_kde_locmix, lfsrs, Zs)
+lfsr_ci_lam_locmix = confint.(lam_kde_locmix, lfsrs, Zs)
 
-low = getproperty.(postmean_ci_dkw_scalemix, :lower)
-upp = getproperty.(postmean_ci_dkw_scalemix, :upper)
+plot([-3;3], [0.5; 0.5], seriestype=:line, linestyle=:dot, label=nothing, color=:lightgrey)
+plot!(ts, lfsr_ci_dkw_locmix, label="KDE-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(0,1))
+plot!(ts, lfsr_ci_dkw_locmix, label="DKW-Loc",show_ribbon=false, alpha=0.9, color=:black)
+plot!(ts, lfsr_ci_lam_locmix, label="Affine minimax",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
+plot!(xlabel = L"x", ylabel=L"\PP{\mu \geq 0 \mid X=x}", size=(380,280))
 
-low2 = getproperty.(postmean_ci_dkw_locmix, :lower)
-upp2 = getproperty.(postmean_ci_dkw_locmix, :upper)
-
-plot(ts, [low upp], xlim=(-3,3))
-plot(1, ts, postmean_ci_dkw_scalemix, label="DKW scale")
-plot!(1, ts, postmean_ci_kde_scalemix, label="KDE scale", fillcolor=:purple)
-
-plot(ts, [low2 upp2], xlim=(-3,3))
-
-pgfplotsx()
-plot(ts, postmean_ci_kde_locmix, label="KDE loc", fillcolor=:blue)
-plot(ts, postmean_ci_lam_locmix, label="KEK",show_ribbon=false, linestyle=:solid, xlim=(-3,3))
-plot!(ts, postmean_ci_lam_scalemix, label="SCALE",show_ribbon=true, xlim=(-3,3))
-
-
-hline!([0.0])
-plot!([-3.0; 3.0], [-3.0;3.0])
+savefig("prostate_locmix_lfsr.tikz")
